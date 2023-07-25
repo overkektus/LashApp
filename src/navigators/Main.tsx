6f0c58f060af47d5dd1fe9fbb917b88de5e16ef7
Egor Piskunou <egor.piskunou@gmail.com>
2023-07-25 22:03:16 +0200

    setup icons in tab nav based on route

--- src/navigators/Main.tsx
+++ src/navigators/Main.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../screens/Home/Home';
 import Statistics from '../screens/Statistics/Statistics';
 import Calendar from '../screens/Calendar/Calendar';
@@ -12,7 +14,30 @@ const MainNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{ headerShown: false }}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = '';
+
+          switch (route.name) {
+            case 'Home':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+            case 'Calendar':
+              iconName = focused ? 'calendar-month' : 'calendar-month-outline';
+              break;
+            case 'Statistics':
+              iconName = focused ? 'chart-box' : 'chart-box-outline';
+              break;
+            case 'Profil':
+              iconName = focused ? 'account' : 'account-outline';
+              break;
+          }
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
     >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Calendar" component={Calendar} />

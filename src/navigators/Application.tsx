93c685de415a1b6b951209654684d83dce701f66
Egor Piskunou <egor.piskunou@gmail.com>
2023-07-25 11:36:04 +0200

    add new screens in to navigation

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -10,6 +10,9 @@ import { useTheme } from '../hooks';
 import MainNavigator from './Main';
 import { useFlipper } from '@react-navigation/devtools';
 import { ApplicationStackParamList } from '../../@types/navigation';
+import Welcome from '../screens/Welcome';
+import Login from '../screens/Login/Login';
+import Signup from '../screens/Signup/Signup';
 
 const Stack = createStackNavigator<ApplicationStackParamList>();
 
@@ -26,8 +29,11 @@ const ApplicationNavigator = () => {
     <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
       <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
         <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{ headerShown: true }}>
           <Stack.Screen name="Startup" component={Startup} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Welcome" component={Welcome} />
           <Stack.Screen name="Main" component={MainNavigator} />
         </Stack.Navigator>
       </NavigationContainer>

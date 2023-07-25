c49ef9ee2ac1d4d18e27cac97aa54b13fea50dd0
Egor Piskunou <egor.piskunou@gmail.com>
2023-07-25 11:48:32 +0200

    add welcome screen[WIP]

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,39 @@
+import { ApplicationScreenProps } from 'LashApp/@types/navigation';
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+
+const Welcome = ({ navigation }: ApplicationScreenProps) => {
+  return (
+    <SafeAreaView>
+      <Text variant="displayLarge">Let's you in</Text>
+      <View>
+        <Button mode="outlined" onPress={() => navigation.navigate('Login')}>
+          Continue with Facebook
+        </Button>
+        <Button mode="outlined" onPress={() => navigation.navigate('Login')}>
+          Continue with Google
+        </Button>
+        <Button mode="outlined" onPress={() => navigation.navigate('Login')}>
+          Continue with Apple
+        </Button>
+      </View>
+      <Button mode="contained" onPress={() => navigation.navigate('Login')}>
+        Sign in with password
+      </Button>
+      <View>
+        <Text>Don't have an account?</Text>
+        <Button mode="text" onPress={() => navigation.navigate('Signup')}>
+          Sign up
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+// const SafeAreaView = styled.SafeAreaView`
+//   display: flex;
+//   align-items: center;
+// `;
+
+export default Welcome;
